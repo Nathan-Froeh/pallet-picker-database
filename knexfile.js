2b8b684c940e4a17629174f5ba94cf059e595d35
Nathan <nathan.froeh@gmail.com>
2019-08-19 14:34:13 -0600

    add knex file Co-authored-by: Colby Allen <colbyallen012@gmail.com>

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/palette_picker',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    },
+    useNullAsDefault: true
+  },
+  
+
+  
+
+};
