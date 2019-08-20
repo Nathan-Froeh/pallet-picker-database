8d4bb5ca0d23966687ce0d318071726e378cb7ca
Nathan <nathan.froeh@gmail.com>
2019-08-20 13:20:46 -0600

    Add GET all projects for specific project Co-authored-by: Colby Allen <colbyallen012@gmail.com>

--- server.js
+++ server.js
@@ -25,8 +25,17 @@ app.get('/api/v1/projects', (request, response) => {
     .catch(error => response.status(500).json({error}))
 });
 
+//TEST
 app.get('/api/v1/palettes', (request, response) => {
   database('palettes').select()
+    .then(palettes => response.status(200).json(palettes))
+    .catch(error => response.status(500).json({error}))
+});
+//TEST
+
+app.get('/api/v1/projects/:id/palettes', (request, response) => {
+  const {id} = request.params;
+  database('palettes').where('project_id', id).select()
     .then(palettes => response.status(200).json(palettes))
     .catch(error => response.status(500).json({error}))
 });
