4b45b1d3864b5bfbea458ab51002b9ba44d31475
Colby Allen <colbyallen012@gmail.com>
2019-08-23 20:12:55 -0600

    add happy path to delete projects

--- app.test.js
+++ app.test.js
@@ -48,4 +48,34 @@ describe('API', () => {
       expect(response.body.error).toEqual(`Could not find palette with id #${invalidId}`)
     })
   })
+
+  // describe('POST /api/v1/palettes', () => {
+  //   it('HAPPY PATH: should post a new palette to the database', async () => {
+  //     const newPalette = {name: 'new pal', color_1: '#FFFFFF', color_2: '#FFFFFF', color_3: '#FFFFFF', color_4: '#FFFFFF', color_5: '#FFFFFF', project_name: 'new proj'}
+  //     const response = await request(app).post('/api/v1/palettes').send(newPalette)
+  //     const pals = await database('palettes').where({ id: response.body.id });
+  //     const newPalettes = pals[0]
+  //     expect(response.status).toBe(200)
+  //     expect(newPalettes.name).toEqual(newPalette.name)
+  //   })
+  // })
+
+  // describe('PATCH /api/v1/palettes/:id', () => {
+  //   it('HAPPY PATH: should return a status of 201 and update a specific palette', async () => {
+  //     const updatePalette = {project_id: 1, name: '#ASDLKJ'};
+  //     const expectedId = await database('palettes').first('id');
+  //     const response = await request(app).patch(`/api/v1/palettes/${expectedId}`).send(updatePalette);
+  //     const newPalette = await database('palettes').where({ id: expectedId })
+  //     expect(response.status).toBe(201)
+  //     expect(newPalette[0].color_one).toEqual(updatePalette.color_1)
+  //   })
+  // })
+
+  describe('DELETE /api/v1/projects/:id', () => {
+    it('should return a status of 204 when a projects is deleted', async () => {
+      const expectedId = await database('projects').first('id').then(object => object.id);
+      const response = await request(app).delete(`/api/v1/projects/${expectedId}`)
+      expect(response.status).toBe(204)
+    })
+  })
 })
